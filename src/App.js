5cae27460cfdc3301e706a3bfc84cef29a362edf
Shehan Etulgama <121471956+Shehan-11@users.noreply.github.com>
2024-12-09 21:58:48 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
+import Tickets from './pages/Tickets';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return <div className="App">
-    <Navbar/>
-    <Home/>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/tickets" element={<Tickets/>}/>
+      </Routes>
+    </Router>
   </div>;     
 }
 export default App;
